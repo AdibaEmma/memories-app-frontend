2eafceedf18e43dbe73a0d055a5e61508b9a7e24
Emmanuel Adiba <eabaagah@gmail.com>
2021-09-04 02:59:00 +0000

    App component design with material ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <Container>
+      <AppBar>
+        <Typography>Memories</Typography>
+      </AppBar>
+      <Grow>
+        <Container>
+          <Grid>
+
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
