2305795b20de07dbd46890dfb44884766d41244e
Emmanuel Adiba <eabaagah@gmail.com>
2021-09-04 11:35:59 +0000

    update

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,19 @@
-export default (state={}. action) => {
-    
+/* eslint-disable import/no-anonymous-default-export */
+import {
+    CREATE_POST,
+    FETCH_POSTS,
+    GET_POST,
+    EDIT_POST,
+    DELETE_POST
+} from '../actions/types'
+
+export default (state=[], action) => {
+    switch (action.type) {
+        case FETCH_POSTS:
+            return 'FETCH_POSTS'
+        case CREATE_POST:
+            return 'CREATE_POST'
+        default:
+            return state
+    }
 }
