5955484d4fc9c0af39903a18eb0b80111f1c042d
Emmanuel Adiba <eabaagah@gmail.com>
2021-09-04 11:21:55 +0000

    postReducer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
-import App from './App';
+import { Provider } from 'react';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 
+import App from './App';
+import reducers from './reducers'
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+  ,
   document.getElementById('root')
 );
 
