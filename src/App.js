8aaa2ba6309b4b72cb77ce720c28ed7218c56753
Emmanuel Adiba <eabaagah@gmail.com>
2021-09-04 03:01:57 +0000

    App component design with material ui update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
 function App() {
   return (
-    <Container>
-      <AppBar>
-        <Typography>Memories</Typography>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">Memories</Typography>
       </AppBar>
       <Grow>
         <Container>
-          <Grid>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
 
           </Grid>
         </Container>
