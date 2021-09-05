8028dba45c488c2b7b7f905012b57d25355c7307
Emmanuel Adiba <eabaagah@gmail.com>
2021-09-05 15:41:09 +0000

    form design

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TextField, Button, Typography, Paper } from '@material-ui/core'
 
 import useStyles from './styles'
- 
+
 const Form = () => {
     const classes = useStyles()
+    const [postData, setPostData] = useState({
+        creator: '',
+        title: '',
+        message: '',
+        tags: [],
+        selectedFile: ''
+    })
+
+    const onInputChange = (e, field) => setPostData({ ...postData, [field]: e.target.value }) 
+
+    const handleSubmit = () => {
+
+    }
+    console.log(postData);
     return (
-        <div>
-            <h1>Form</h1>
-        </div>
+        <Paper className={classes.paper}>
+            <form action="" autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+                <Typography variant="h6">Create a Memory</Typography>
+                <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={e => onInputChange(e, 'creator') } />
+                <TextField name="creator" variant="outlined" label="Title" fullWidth value={postData.title} onChange={e => onInputChange(e, 'title') } />
+                <TextField name="creator" variant="outlined" label="Message" fullWidth value={postData.message} onChange={e => onInputChange(e, 'message') } />
+                <TextField name="creator" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={e => onInputChange(e, 'tags') } />
+            </form>
+        </Paper>
     )
 }
 
