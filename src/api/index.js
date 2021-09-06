785e135abe355c1445e35970b0b284806ef2244e
Emmanuel Adiba <eabaagah@gmail.com>
2021-09-06 09:05:05 +0000

    api update

--- src/api/index.js
+++ src/api/index.js
@@ -7,3 +7,5 @@ export const fetchPosts = () => axios.get(url)
 export const createPost = (newPost) => axios.post(url, newPost)
 
 export const getPost = (id) => axios.get(`${url}/${id}`)
+
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost)
