5776e711c0d03395e1796a496dff52e8b3dafc6a
Emmanuel Adiba <eabaagah@gmail.com>
2021-09-07 02:49:39 +0000

    update

--- src/components/Posts/Post/Post.jsx
+++ src/components/Posts/Post/Post.jsx
@@ -30,7 +30,7 @@ const Post = ({ post, setCurrentId }) => {
             </div>
             <Typography className={classes.title} variant="h5" color="textSecondary" gutterBottom>{post.title}</Typography>
             <CardContent>
-                <Typography variant="body2" color="textSecondary">{post.message}</Typography>
+                <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
                 <Button size="small" color="primary" onClick={() => dispatch(like_post(post._id))}>
