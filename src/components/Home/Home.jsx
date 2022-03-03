3539e78883fc947e55c9dd25dcaa7944855538ed
Emmanuel Adiba <eabaagah@gmail.com>
2022-03-03 20:06:55 +0000

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,29 +8,29 @@ import Form from '../Form/Form';
 import { fetch_posts } from '../../actions/posts';
 
 const Home = () => {
-    const classes = useStyles()
-    const dispatch = useDispatch()
-  
-    const [currentId, setCurrentId] = useState(null)
-  
-    useEffect(() => {
-      dispatch(fetch_posts())
-    }, [currentId, dispatch])
+  const classes = useStyles()
+  const dispatch = useDispatch()
 
-    return (
-        <Grow in>
-        <Container>
-          <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
+  const [currentId, setCurrentId] = useState(null)
+
+  useEffect(() => {
+    dispatch(fetch_posts())
+  }, [currentId, dispatch])
+
+  return (
+    <Grow in>
+      <Container>
+        <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
+          <Grid item xs={12} sm={7}>
+            <Posts setCurrentId={setCurrentId} />
           </Grid>
-        </Container>
-      </Grow>
-    )
+          <Grid item xs={12} sm={4}>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+          </Grid>
+        </Grid>
+      </Container>
+    </Grow>
+  )
 }
 
 export default Home
